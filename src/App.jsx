58500aa626621fde31e6d35176689fa42c23b6af
Нщлги <yoqub.madixanov@outlook.com>
2025-08-15 00:17:52 +0500

    Making reset, login, sign up pages adaptive

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/login/Login";
 import ResetPage from "./pages/reset/Reset";
 import RegisterPage from "./pages/registration/Register";
@@ -7,10 +7,13 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* При заходе на / — сразу редирект на /login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<RegisterPage />} />
         <Route path="/reset-password" element={<ResetPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
